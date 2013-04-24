25ef3b98e41716d47c2f4e6abefcf16ef3d85b57
Viktor Nagy <viktor.nagy@gmail.com>
2013-04-24 14:54:54 +0200

    ActivateSubscription is csrf_exempt

--- django_fastspring/urls.py
+++ django_fastspring/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 from .views import ActivateSubscription
 
 urlpatterns = patterns('',
-    url(r'^subscriptions/activated/$', ActivateSubscription.as_view(), name='fastspring_subscription_activate'),
+    url(r'^subscriptions/activated/$', csrf_exempt(ActivateSubscription.as_view()), name='fastspring_subscription_activate'),
 )
